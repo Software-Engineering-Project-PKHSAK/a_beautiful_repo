a625600cf6fd2990707dca795fb9bfb58667b820
himanshu1603 <himanshu30ber@gmail.com>
2024-08-25 17:41:59 -0400

    adding first py file

---
+++ myFile.py
@@ -0,0 +1,11 @@
+def calculate_factorial(n):
+    if n==0 or n==1:
+        return 1
+    else: 
+        return n*calculate_factorial(n-1);
+
+number = int(input("Enter a number to calculate its factorial: "))
+factorial = calculate_factorial(number)
+print(f"The factorial of {number} is: {factorial}")
+
+result = factorial / 0
