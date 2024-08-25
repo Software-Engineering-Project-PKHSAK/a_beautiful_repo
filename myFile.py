07456190439a2c3951f27e33e9f913323fe69494
himanshu1603 <himanshu30ber@gmail.com>
2024-08-25 22:35:07 +0000

    Added unit tests

--- myFile.py
+++ myFile.py
@@ -4,8 +4,8 @@ def calculate_factorial(n):
     else: 
         return n*calculate_factorial(n-1);
 
-number = int(input("Enter a number to calculate its factorial: "))
-factorial = calculate_factorial(number)
-print(f"The factorial of {number} is: {factorial}")
+# number = int(input("Enter a number to calculate its factorial: "))
+# factorial = calculate_factorial(number)
+# print(f"The factorial of {number} is: {factorial}")
 
-result = factorial / 0
+# result = factorial / 0
---
+++ test_myFile.py
@@ -0,0 +1,8 @@
+from myFile import calculate_factorial
+
+def test_calculate_factorial():
+    assert(calculate_factorial(5)==120)
+    assert(calculate_factorial(6)==720)
+
+def test_calculate_factorial_negative():
+    assert(calculate_factorial(-2)== "No real number exists")
